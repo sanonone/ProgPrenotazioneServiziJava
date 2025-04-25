79e617e23889546b50e29491088d535468427f60
sanonone <fedeld023@gmail.com>
2025-04-25 12:22:00 +0200

    25 apr commit da portatile

--- src/main/java/com/example/spring/spring/client/Client.java
+++ src/main/java/com/example/spring/spring/client/Client.java
@@ -2,8 +2,15 @@ package com.example.spring.spring.client;
 
 import java.net.http.HttpClient;
 import java.time.Duration;
+import java.time.LocalTime;
 
+import com.example.spring.spring.model.persona.Cliente;
+import com.example.spring.spring.model.persona.Utente;
+import com.example.spring.spring.model.prenotazioneServizio.PrenotazioneServizio;
+import com.example.spring.spring.model.prenotazioneServizio.PrenotazioneServizioOrario;
+import com.example.spring.spring.model.servizio.ServiziGiornalieri;
 import com.example.spring.spring.model.servizio.ServiziOrari;
+import com.example.spring.spring.model.servizio.TimeInterval;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
@@ -35,29 +42,30 @@ public class Client {
 
 
 
-            /*
-            chiamateCliente.creaCliente(new Cliente("gigi", "proiettili", 23, 348578387,"mario.rossi@example.com"));
-            chiamateCliente.getClienteById("67fbb21307b3a35264c9587a");
-            chiamateCliente.getAllClienti();
-            chiamateCliente.deleteCliente("67fbb21307b3a35264c9587a");
-             */
+
+            //chiamateCliente.creaCliente(new Cliente("Mario", "Rossi", 23, 348578387,"mario.rossi@example.com"));
+            //chiamateCliente.creaCliente(new Cliente("Luigi", "Verdi", 39, 348578387,"luigi.verdi@example.com"));
+            //chiamateCliente.getClienteById("67fbb21307b3a35264c9587a");
+            //chiamateCliente.getAllClienti();
+            //chiamateCliente.deleteCliente("67fbb21307b3a35264c9587a");
 
 
-            /*
-            chiamateUtente.creaUtente(new Utente("Pippo", "Rossi", 23, 348578387,"pippo","pw","receptionist","02-02-2021"));
-            chiamateUtente.getAllUtenti();
-            chiamateUtente.getUtenteById("6808f8783694b10175a8aae1");
-            chiamateUtente.deleteUtente("67fe64883a8df7041bc2bbdb");
-             */
 
-            /*
-            chiamateServiziGiornalieri.creaServizioGiornaliero(new ServiziGiornalieri("test", "motorini da noleggiare per l'intero giorno o più", 20.00, 10));
-            chiamateServiziGiornalieri.getAllServiziGiornalieri();
-            chiamateServiziGiornalieri.getServiziGiornalieriById("67ff9f76d550d46d1e8ed1e4");
-            chiamateServiziGiornalieri.deleteServizioGiornaliero("67ffa71ed550d46d1e8ed1e8");
-             */
 
-            //chiamatePrenotazioneGiornaliera.creaPrenotazioneGiornaliera(new PrenotazioneServizio("67ffa52dd550d46d1e8ed1e5", "67fe41d2e1c5dc459f047a22", "Noleggio motorino", "Mario", "Rossi", "15/01/2026", 3, 1,10));
+            //chiamateUtente.creaUtente(new Utente("Stefano", "Gialli", 36, 348578387,"gilli","pw","receptionist","02/02/2021"));
+            //chiamateUtente.getAllUtenti();
+            //chiamateUtente.getUtenteById("6808f8783694b10175a8aae1");
+            //chiamateUtente.deleteUtente("67fe64883a8df7041bc2bbdb");
+
+
+
+            //chiamateServiziGiornalieri.creaServizioGiornaliero(new ServiziGiornalieri("Noleggio motorini", "motorini da noleggiare per l'intero giorno o più", 20.00, 10));
+            //chiamateServiziGiornalieri.getAllServiziGiornalieri();
+            //chiamateServiziGiornalieri.getServiziGiornalieriById("67ff9f76d550d46d1e8ed1e4");
+            //chiamateServiziGiornalieri.deleteServizioGiornaliero("67ffa71ed550d46d1e8ed1e8");
+
+
+            //chiamatePrenotazioneGiornaliera.creaPrenotazioneGiornaliera(new PrenotazioneServizio("680a13a5e6356128547377db", "680a12e3e6356128547377d7", "Noleggio motorini", "Mario", "Rossi", "15/01/2026", 3, 1,60));
 
             // 1. Creare i singoli oggetti TimeInterval
             /*
@@ -74,13 +82,14 @@ public class Client {
             chiamateServiziOrari.creaServizioOrario(new ServiziOrari("Massaggi", "massaggio bellissimo", 100.00, fascePerServizio, 5));
             chiamateServiziOrari.getAllServiziOrari();
              */
+            //chiamateServiziOrari.creaServizioOrario(new ServiziOrari("Prenota ombrellone", "prenotazione ombrellone spiaggia per una fascia oraria", 10.00, null, 5));
 
             /*
             TimeInterval fasciaMattina = new TimeInterval(LocalTime.of(9, 0), LocalTime.of(11, 0)); // 09:00 - 11:00
             chiamatePrenotazioneOraria.creaPrenotazioneOraria(new PrenotazioneServizioOrario("680511a9c398b93a6c34eaa3", "67fe64883a8df7041bc2bbdb", "Massaggio", "Mario", "Rossi", "15/01/2026", 2, 2, 100, fasciaMattina));
             */
 
-            chiamateServiziOrari.creaServizioOrario(new ServiziOrari("no fasce", "massaggio bellissimo", 100.00, null, 5));
+            //chiamateServiziOrari.creaServizioOrario(new ServiziOrari("no fasce", "massaggio bellissimo", 100.00, null, 5));
 
         } catch (Exception e) {
             // Gestione di errori (es. server non raggiungibile, errori di rete)
